227ad1e55fbe880b46d0dd916541a467f300f7fc
Marc Gomez <marcgomez@microsoft.com>
2021-12-22 11:29:53 +0100

    Add script to automate provisioning and deployment

---
+++ distributed-calculator/deploy.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+set -euo pipefail
+
+run_terraform() {
+    terraform init
+    terraform plan
+    terraform apply --auto-approve
+}
+
+echo "Provisioning AKS cluster..."
+(cd terraform/aks; run_terraform)
+echo "DONE"
+
+echo
+
+echo "Deploying dapr and distributed calculator application..."
+(cd terraform/aks; run_terraform)
+echo "DONE"
